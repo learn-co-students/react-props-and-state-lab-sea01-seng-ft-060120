2759aa8fc02136ff38323bd9bfb811201f03282d
cortezd334 <cortezd334@gmail.com>
2020-09-16 13:19:53 -0700

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,46 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  petData = () => {
+    return this.props.pets.map(pet => {
+      // return <Pet pets={pet}/>
+      return <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet}/>
+    })
+  }
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+
+    return (
+    <div className="ui cards">{this.petData()}</div>
+    )
   }
 }
 
 export default PetBrowser
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import getAll from '../data/pets'
+// import Pet from './Pet'
+
+// class PetBrowser extends React.Component {
+
+  
+//   render() {
+//     return this.props.pets.map(pet => <Pet pet={pet}/>)
+//     console.log(this.props.pets)
+//     // return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+//   }
+// }
+
+// export default PetBrowser
